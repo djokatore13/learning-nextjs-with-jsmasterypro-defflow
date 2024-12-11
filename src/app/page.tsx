a6ab54a7cfb61b30c2d9aa2acf460f6052aedcba
Djordje Stojanovic <djordje@breyta.is>
2024-12-11 22:08:23 +0100

    Setup assets and metadata.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 export default function Home() {
   return (
-    <h1 className="h1-bold">Welcome to the world of NextJS</h1>
+    <>
+      <h1 className="h1-bold font-space-grotesk">Welcome to the world of NextJS</h1>
+      <h2 className="h2-bold">Welcome to the world of NextJS</h2>
+    </>
   );
 }
