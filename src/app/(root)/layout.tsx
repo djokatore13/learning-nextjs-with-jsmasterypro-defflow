7fa0be5d62a9ffb53de28a0d39ce00f0d0686f8b
Djordje Stojanovic <djordje@breyta.is>
2025-01-06 17:34:39 +0100

    implemented lest and right sidebar.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 // Components
 import Navbar from "@/components/navigation/navbar";
+import LeftSidebar from '@/components/navigation/LeftSidebar';
+import RightSidebar from '@/components/navigation/RightSidebar';
 
 type Props = {
   children: React.ReactNode
@@ -9,9 +11,21 @@ type Props = {
 
 export default function RootLayout({ children }: Props) {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
+
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">
+            {children}
+          </div>
+        </section>
+
+        <RightSidebar />
+      </div>
+
     </main>
   )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { techNameMap } from "../../constants"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const getDeviconClassName = (techName: string) => {
+  const normalizedTechName = techName.replace(/[ .]/g, '').toLowerCase();
+
+  return techNameMap[normalizedTechName] ? `${techNameMap[normalizedTechName]} colored` : 'devicon-devicon-plain';
 }
